3d7247c469b07d83df40178b02602591d60bfbbe
Bo Latt <bolattny@gmail.com>
2022-11-14 19:51:21 -0500

    add drinks page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./components/Main";
+import Drinks from "./components/Drinks";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />}></Route>
+          <Route path="/drinks" element={<Drinks />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/components/Drinks.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { drinksData } from "../data/drinksData";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+    "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+  },
+};
+
+export default function Drinks() {
+  const [drinks, setDrinks] = useState([]);
+  useEffect(() => {
+    fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setDrinks(response.drinks);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="drinks">
+      {drinks.map((drink, i) => (
+        <div key={i} className="card">
+          <img src={drink.strDrinkThumb} alt="" />
+          <div>
+            <h3>{drink.strDrink}</h3>
+            <p>Category: {drink.strCategory}</p>
+            <p>Instructions: {drink.strInstructions}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
